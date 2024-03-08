6e3ab4e6d9b58d987cf0f731a4dfc692841087e7
Antonio <63789531+dumitrutony03@users.noreply.github.com>
2024-03-08 11:29:14 +0200

    .

--- src/main/java/com/example/raiffaisen/Controller/InvestorController.java
+++ src/main/java/com/example/raiffaisen/Controller/InvestorController.java
@@ -2,6 +2,7 @@ package com.example.raiffaisen.Controller;//package com.example.raiffaisen.Contr
 
 import com.example.raiffaisen.DTO.LoginDTO;
 import com.example.raiffaisen.Service.ServiceInvestor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,16 +10,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
+@CrossOrigin(origins="*", allowedHeaders = "*")
 @RequestMapping("/investor")
+@RequiredArgsConstructor
 public class InvestorController {
-    private final ServiceInvestor investorService;
 
-    //    @Autowired
-//    private Authentication authenticationManager;
+    @Autowired
+    private ServiceInvestor serviceInvestor;
     @Autowired
     public InvestorController(ServiceInvestor serviceInvestor) {
-        this.investorService = serviceInvestor;
-        System.out.println("Luam investitorii folosind REST + numarul de investitori inregistrati este: " + investorService.getAllEntities().size());
+        this.serviceInvestor = serviceInvestor;
     }
 
     @GetMapping
@@ -43,25 +44,14 @@ public class InvestorController {
     catre un link de genul '/login', si aici isi introduce adresa de email si parola,
     apoi noi folosim algoritmul in care verificam daca exista Investorul asta in Baza de date
     * */
-
-    //    @PostMapping("/login") // @ModelAttribute
-//    public ResponseEntity<?> investorLogin(@RequestAttribute LoginDTO loginDTO) {
-//        System.out.println("Metoda apelata din web");
-//
-//        boolean investor = investorService.InvestorLogin(loginDTO.getEmail(), loginDTO.getPassword());
-//        System.out.println("Investor logat cu succes");
-//        if (investor) {
-//            return ResponseEntity.ok().body("Investor logat cu succes!");
-//            // Într-o aplicație reală, aici s-ar returna un token JWT sau alt mecanism de sesiune
-//        } else {
-//            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Credențiale invalide!");
-//        }
     @PostMapping("/login")
-    public ResponseEntity<?> login(@ModelAttribute LoginDTO loginDTO) {
+    public ResponseEntity<?> login(@RequestBody LoginDTO loginDTO) {
+        System.out.println("LoginDTO + Luam investitorii folosind REST + numarul de investitori inregistrati este: " + this.serviceInvestor.getAllEntities().size());
+
         // Logica de autentificare
         System.out.println(loginDTO.getEmail() + " " + loginDTO.getPassword());
 
-        boolean investor = investorService.InvestorLogin(loginDTO.getEmail(), loginDTO.getPassword());
+        boolean investor = serviceInvestor.InvestorLogin(loginDTO.getEmail(), loginDTO.getPassword());
         if (investor) {
             return ResponseEntity.ok().body("Investor logat cu succes!");
             // Într-o aplicație reală, aici s-ar returna un token JWT sau alt mecanism de sesiune
--- src/main/java/com/example/raiffaisen/RaiffaisenApplication.java
+++ src/main/java/com/example/raiffaisen/RaiffaisenApplication.java
@@ -8,8 +8,6 @@ import com.example.raiffaisen.Service.ServiceInvestor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class RaiffaisenApplication {
@@ -19,19 +17,6 @@ public class RaiffaisenApplication {
         ServiceInvestor serviceInvestor = new ServiceInvestor(investorSQLRepository);
 
         /// Controllers
-//        InvestorController investorController = new InvestorController(serviceInvestor);
         System.out.println(serviceInvestor.InvestorLogin("alexvescan16@yahoo.com", "Messi2016"));
-        /*
-//        System.out.println(serviceInvestor.InvestorLogin("investor1@yahoo.com", "Investor1Password"));
-
-        System.out.println(serviceInvestor.InvestorRegister("Investor1", 21, "investor1@yahoo.com", "Investor1Password", 10000));
-
-//        IRepository<Startup> startupSQLRepository = new StartupSQLRepository<>();
-//        IService<Startup> startupService = new Service<>(startupSQLRepository);
-
-        System.out.println("Afisam investitorii din DB");
-        for (var investor : investorServiceInvestor.getAllEntities()) {
-            System.out.println(investor.toString());
-        }*/
     }
 }
